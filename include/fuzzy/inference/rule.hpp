e1139c5c80d910a77542107fb9120c039f03788b
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2018-11-11 21:52:17 +0100

    Refactored rule.hpp

--- include/fuzzy/inference/rule.hpp
+++ include/fuzzy/inference/rule.hpp
@@ -10,21 +10,14 @@ namespace fuzzy
 class Rule
 {
 public:
-    Rule
-    (
-        std::vector< Set > antecedent,
-        Set                consequent
-    ) :
-        antecedent_{ antecedent },
-        consequent_{ consequent }
-    {}
+    Rule( Sets antecedent, Set consequent ) : antecedent_{ antecedent }, consequent_{ consequent } {}
 
     auto const & antecedent() const { return antecedent_; }
     auto const & consequent() const { return consequent_; }
 
 private:
-    std::vector< Set > antecedent_;
-    Set                consequent_;
+    Sets antecedent_;
+    Set  consequent_;
 };
 
 using Rules = std::vector< Rule >;
