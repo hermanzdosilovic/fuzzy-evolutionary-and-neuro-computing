e9e0cba21e4bad8c2de87d86ed0c1d32e1a80755
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2018-11-11 23:29:56 +0100

    Simplified rules.

--- src/homework_03/task_01/rules.hpp
+++ src/homework_03/task_01/rules.hpp
@@ -9,36 +9,17 @@ fuzzy::Rules accelerationRules()
 {
     fuzzy::Rules rules;
 
-    fuzzy::Domain const u{ fuzzy::Domain::Range(   0, 2 ) };
-    fuzzy::Domain const v{ fuzzy::Domain::Range( -10, 10 ) };
+    fuzzy::Domain const u{ fuzzy::Domain::Range(   0, 100 ) };
+    fuzzy::Domain const v{ fuzzy::Domain::Range( -10,  10 ) };
 
     fuzzy::Rule r1
     {
         {
-            fuzzy::Set{ u, fuzzy::lambda( 25, 50, 100 ) },
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set{ u, fuzzy::lambda( 5, 10, 10 ) },
-            fuzzy::Set::Universal(),
+            fuzzy::Set{ u, fuzzy::lambda( 0, 50, 100 ) }
         },
-        fuzzy::Set{ v, fuzzy::lambda( -10, -5, 0 ) }
+        fuzzy::Set{ v, fuzzy::lambda( 0, 5, 10 ) }
     };
     rules.emplace_back( r1 );
-
-    fuzzy::Rule r2
-    {
-        {
-            fuzzy::Set::Universal(),
-            fuzzy::Set{ u, fuzzy::lambda( 25, 50, 100 ) },
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set{ u, fuzzy::lambda( 5, 10, 10 ) },
-            fuzzy::Set::Universal(),
-        },
-        fuzzy::Set{ v, fuzzy::lambda( -10, -5, 0 ) }
-    };
-    rules.emplace_back( r2 );
 
     return rules;
 }
@@ -47,36 +28,17 @@ fuzzy::Rules rudderRules()
 {
     fuzzy::Rules rules;
 
-    fuzzy::Domain const u{ fuzzy::Domain::Range(   0, 1301 ) };
-    fuzzy::Domain const v{ fuzzy::Domain::Range( -90,   90 ) };
+    fuzzy::Domain const u{ fuzzy::Domain::Range(   0, 100 ) };
+    fuzzy::Domain const v{ fuzzy::Domain::Range( -90,  90 ) };
 
     fuzzy::Rule r1
     {
         {
-            fuzzy::Set{ u, fuzzy::lambda( 25, 50, 100 ) },
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-        },
-        fuzzy::Set{ v, fuzzy::lambda( -90, -45, 0 ) }
-    };
-    rules.emplace_back( r1 );
-
-    fuzzy::Rule r2
-    {
-        {
-            fuzzy::Set::Universal(),
-            fuzzy::Set{ u, fuzzy::lambda( 25, 50, 100 ) },
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
-            fuzzy::Set::Universal(),
+            fuzzy::Set{ u, fuzzy::lambda( 0, 50, 100 ) }
         },
         fuzzy::Set{ v, fuzzy::lambda( 0, 45, 90 ) }
     };
-    rules.emplace_back( r2 );
+    rules.emplace_back( r1 );
 
     return rules;
 }
